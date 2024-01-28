707f2093da7654a47389f7e8baf51729cb1cc9cd
Amirhossein Azimyzadeh <Amirhossein.azimyzadeh@outlook.com>
2024-01-28 15:35:01 -0400

    add ts hand tracking packages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,37 @@
-import './style.css'
+import initiateTracker from './initiateTracker';
+import initiateCanvasElement from './initiateCanvasElement';
+import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
+import drawBoundingBox, { Point } from './drawBoundingBox';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    Web Object Tracking
-  </div>
-`
+import './style.css';
+
+
+function main() {
+  const [canvas, video] = initiateCanvasElement();
+
+  // add video element to DOM
+  const parent = document.createElement('div');
+  parent.classList.add('video-container');
+  parent.appendChild(video);
+  parent.appendChild(canvas);
+  document.getElementById('app').appendChild(parent);
+
+  initiateTracker().then((tracker: handPoseDetection.HandDetector | null) => {
+    if (tracker === null) return;
+
+    const frameHandler = () => {
+      tracker.estimateHands(video).then((hands) => {
+        const hand2DPoints: Array<Array<Point>> = hands.map((hand) => {
+          return hand.keypoints.map((k) => ({ x: k.x, y: k.y }))
+        });
+        drawBoundingBox(hand2DPoints, canvas);
+        requestAnimationFrame(frameHandler);
+      });
+    }
+
+    frameHandler();
+
+  });
+}
+
+main();
